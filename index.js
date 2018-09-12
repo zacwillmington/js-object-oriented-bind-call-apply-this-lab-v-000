35c3dc96e70d669be09602fb511d06b8102e8064
Zac Willmington <zac.willmington@gmail.com>
2018-09-12 23:18:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke (arg) {
+    return arg.call(this);
+ }
+
+ function setThisWithCall (func, name, age){
+     return func.call(name, age);
+ }
+
+function setThisWithApply (func, name, args){
+    return func.apply(name, args);
+}
+
+function returnNewFunctionOf (func, name) {
+    return func.bind(name);
+}
